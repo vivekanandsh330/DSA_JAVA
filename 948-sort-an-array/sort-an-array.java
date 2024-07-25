9aa72f214e677706e1adc6e600f287fbc94c335b
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-07-25 20:03:53 +0530

    Time: 171 ms (14.25%) | Memory: 56.1 MB (48.41%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -4,14 +4,14 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
-        int ans[]=new int[nums.length];
+       // int ans[]=new int[nums.length];
         int i=0;
         for(int n: hm.keySet()){
             int c=hm.get(n);
             while(c-->0){
-                ans[i++]=n;
+                nums[i++]=n;
             }
         }
-        return ans;
+        return nums;
     }
 }
