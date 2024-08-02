da4651e593ef427af158cb70e7690b07c92a8e21
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-08-02 11:10:42 +0530

    Time: 16 ms (7.39%) | Memory: 58.8 MB (88.39%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,21 +1,17 @@
 class Solution {
     public int minSwaps(int[] nums) {
         int n=nums.length;
-        int arr[]=new int[2*n];
-        for(int i=0;i<2*n;i++){
-            arr[i]=nums[i%n];
-        }
         int TotalnoOnes=Arrays.stream(nums).sum();
         int i=0;
         int j=0;
         int currOne=0;
         int maxOne=0;
         while(j< 2*n){
-            if(arr[j] == 1){
+            if(nums[j%n] == 1){
                 currOne++;
             }
             if(j-i+1 > TotalnoOnes){
-                currOne -= arr[i];
+                currOne -= nums[i%n];
                 i++;
             }
             j++;
