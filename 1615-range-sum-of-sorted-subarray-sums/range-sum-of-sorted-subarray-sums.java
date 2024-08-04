7e6d2abc545016ba2ec1526f9165c47a45db7d44
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-08-04 16:24:17 +0530

    Time: 83 ms (39.64%) | Memory: 44.7 MB (82.43%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,20 +1,21 @@
 class Solution {
+    int m=(int)1e9+7;
     public int rangeSum(int[] nums, int n, int left, int right) {
-        ArrayList<Integer> prefixsum=new ArrayList<>();
-        int k=0;
+        PriorityQueue<int[]> mh=new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
         for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-               sum+=nums[j];
-               prefixsum.add(sum);
+            mh.add(new int[]{nums[i],i});
+        }
+
+        int count=0;
+        for(int i=1;i<=right;i++){
+            int arr[]=mh.poll();
+            if(i>=left){
+                count = (count+arr[0])%m;
+            }
+            if(arr[1] < n-1){
+                mh.add(new int[]{arr[0]+nums[arr[1]+1],arr[1]+1});
             }
         }
-        Collections.sort(prefixsum);
-        int ans=0;
-        int mod=(int)1e9+7;
-        for(int i=left-1;i<right;i++){
-            ans =(ans+prefixsum.get(i))%mod;
-        }
-        return ans;
+        return count;
     }
 }
