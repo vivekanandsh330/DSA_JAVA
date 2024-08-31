2a5b9891d29ace45a106af7cf054bbd8f1af0741
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-08-31 23:09:23 +0530

    Time: 45 ms (24.55%) | Memory: 59.6 MB (5.08%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,46 +1,40 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-          
-          Map<Integer,List<Pair<Integer,Double>>> graph=new HashMap<>();
+        Map<Integer,List<Pair<Integer,Double>>>hm=new HashMap<>();
 
-          for(int i=0;i<edges.length;i++){
+        for(int i=0;i<edges.length;i++){
             int u=edges[i][0];
             int v=edges[i][1];
-            double pathprob = succProb[i];
-            graph.computeIfAbsent(u,k -> new ArrayList<>()).add(new Pair<>(v,pathprob));
-            graph.computeIfAbsent(v,k -> new ArrayList<>()).add(new Pair<>(u,pathprob));
-          }
+            double prob=succProb[i];
+            hm.computeIfAbsent(u,k->new ArrayList<>()).add(new Pair<>(v,prob));
+            hm.computeIfAbsent(v,k->new ArrayList<>()).add(new Pair<>(u,prob));
+        }
 
-          double ans[]=new double[n];
-          ans[start_node]=1d;
+        double ans[]=new double[n];
+        ans[start_node]=1d;
 
+        PriorityQueue<Pair<Double,Integer>> pq=new PriorityQueue<>((p1,p2)-> p2.getKey().compareTo(p1.getKey()));
+        pq.add(new Pair<>(1d,start_node));
 
-          PriorityQueue<Pair<Double,Integer>> maxp=new PriorityQueue<>(
-            (p1,p2)-> p2.getKey().compareTo(p1.getKey()));
-            maxp.add(new Pair<>(1.0,start_node));
+        while(!pq.isEmpty()){
+            Pair<Double,Integer> curr=pq.poll();
+            double currp=curr.getKey();
+            int currn=curr.getValue();
 
-            while(!maxp.isEmpty()){
-
-                Pair<Double,Integer> cur=maxp.poll();
-                double curProb=cur.getKey();
-                int curNode=cur.getValue();
-
-                if(curNode == end_node){
-                    return curProb;
-                }
-
-                for(Pair<Integer,Double> next: graph.getOrDefault(curNode,new ArrayList<>())){
-                    int nextNode=next.getKey();
-                    double nextProb=next.getValue();
-
-                    if(curProb*nextProb > ans[nextNode]){
-                        ans[nextNode]=curProb*nextProb;
-                        maxp.add(new Pair<>(ans[nextNode],nextNode));
-                    }
-                }
+            if(currn == end_node){
+                return currp;
             }
+            for(Pair<Integer,Double> neigh:hm.getOrDefault(currn,new ArrayList<>())){
+                int nexn=neigh.getKey();
+                double nexp=neigh.getValue();
 
-            return 0d;
-            
+                if(currp*nexp > ans[nexn]){
+                    ans[nexn]=currp*nexp;
+                    pq.add(new Pair<>(ans[nexn],nexn));
+                }
+
+            }
+        }
+        return 0d;
     }
 }
