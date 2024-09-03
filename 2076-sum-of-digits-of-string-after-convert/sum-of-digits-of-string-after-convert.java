2d4d58d41ca4926f300262ee1aee944807db734d
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-09-03 23:56:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (80.62%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int transform(int t) {
+        int res = 0;
+        while (t > 0) {
+            res += t % 10;
+            t /= 10;
+        }
+        return res;
+    }
+    public int getLucky(String s, int k) {
+        int res = 0;
+        for (char ch : s.toCharArray()) {
+            int num = ch - 'a' + 1;
+            if (num >= 10) {
+                res += num % 10 + num / 10;
+            } else {
+                res += num;
+            }
+        }
+        while (--k > 0) {
+            res = transform(res);
+        }
+        return res;
+    }
+}
