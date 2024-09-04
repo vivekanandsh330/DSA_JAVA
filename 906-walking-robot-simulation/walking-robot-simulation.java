408ceb07862be7a9ffb0aac4b9784c98c7a8adf0
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-09-04 22:26:20 +0530

    Time: 38 ms (28.38%) | Memory: 50.6 MB (33.62%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        Set<String> obstecleSet=new HashSet<>();
+        for(int[] obs : obstacles){
+            String key=obs[0]+"_"+obs[1];
+            obstecleSet.add(key);
+        }
+
+        int x=0;
+        int y=0;
+        int maxDistance=0;
+
+        int[] dir={0,1};
+
+        for(int i=0;i<commands.length;i++){
+            if(commands[i] == -2){
+                dir=new int[]{-dir[1],dir[0]};
+            }
+            else if(commands[i] == -1){
+                dir=new int[]{dir[1],-dir[0]};
+            }
+            else{
+                for(int step =0;step<commands[i];step++){
+                    int newX=x+dir[0];
+                    int newY=y+dir[1];
+
+                    String nextKey=newX+"_"+newY;
+
+                    if(obstecleSet.contains(nextKey)){
+                        break;
+                    }
+                    x=newX;
+                    y=newY;
+                }
+            }
+            maxDistance=Math.max(maxDistance, x*x+y*y);
+        }
+        return maxDistance;
+    }
+}
