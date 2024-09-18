c19fc7cb4378c5a7d28585fa702da9a5e9440dc6
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-09-19 00:11:53 +0530

    Time: 5 ms (98.24%) | Memory: 43.1 MB (65.61%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -4,13 +4,14 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             str[i]=Integer.toString(nums[i]);
         }
-        Arrays.sort(str,new Comparator<String>(){
-        public int compare(String a,String b){
-                String f=a+b;
-                String s=b+a;
-                return s.compareTo(f);
-            }
-        });
+        // Arrays.sort(str,new Comparator<String>(){
+        // public int compare(String a,String b){
+        //         String f=a+b;
+        //         String s=b+a;
+        //         return s.compareTo(f);
+        //     }
+        // });
+        Arrays.sort(str,(a,b)->(b+a).compareTo(a+b));
         if(str[0].equals("0")){
             return "0";
         }
