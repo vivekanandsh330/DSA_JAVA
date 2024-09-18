761899cae7ca6fbecaff0ec204a8c1454507a90b
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-09-18 23:16:05 +0530

    Time: 18 ms (80.48%) | Memory: 55.7 MB (65.81%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,17 +1,6 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        TreeMap<Integer,Integer> hm=new TreeMap<>();
-        for(int i=0;i<nums.length;i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-        }
-       // int ans[]=new int[nums.length];
-        int i=0;
-        for(int n: hm.keySet()){
-            int c=hm.get(n);
-            while(c-->0){
-                nums[i++]=n;
-            }
-        }
+        Arrays.sort(nums);
         return nums;
     }
 }
