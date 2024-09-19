99555935b5e9e82da70be0c961fe65d3f7871e07
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-09-19 13:05:36 +0530

    Time: 84 ms (28.66%) | Memory: 58 MB (12.96%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,33 +1,38 @@
 class Solution {
-    Boolean dp[][];
     public boolean canPartition(int[] nums) {
         int sum=0;
         int n=nums.length;
         for(int num: nums){
             sum+=num;
         }
-        dp=new Boolean[n+1][(sum/2)+1];
         if(sum%2 !=0){
             return false; 
         }
         return subsetSum(nums,sum/2,n);
     }
     boolean subsetSum(int []nums,int target,int n){
-       if( n==0 && target>0){
-        return false;
-       }
-        if(target == 0){
-            return true;
+      Boolean dp[][]=new Boolean[n+1][target+1];
+      for(int i=0;i<n+1;i++){
+        for(int j=0;j<target+1;j++){
+            if(i == 0){
+                dp[i][j]= false;
+            }
+            if(j == 0){
+            dp[i][j]= true;
+            }
         }
-        if(dp[n][target] != null){
-            return dp[n][target];
-        }
-        if(nums[n-1]<=target){
-            return dp[n][target]=((subsetSum(nums,target-nums[n-1],n-1) )|| (subsetSum(nums,target,n-1)));
-        }
-        else{
-            return dp[n][target]= subsetSum(nums,target,n-1);
-        }
+      }
+      for(int i=1;i<n+1;i++){
+        for(int j=1;j<target+1;j++){
+            if(nums[i-1]<=j){
+                dp[i][j]= ((dp[i-1][j-nums[i-1]]) || (dp[i-1][j]));
+            }
+            else{
+                dp[i][j]= dp[i-1][j];
+            }
+      }
+      }
+      return dp[n][target];
         
     }
 }
