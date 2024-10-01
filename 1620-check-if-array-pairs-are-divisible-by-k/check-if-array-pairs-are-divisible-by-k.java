5a91a162912140ef24c60d39b58b5c539618b0cd
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-10-01 11:26:34 +0530

    Time: 32 ms (52.67%) | Memory: 59.3 MB (11.11%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
-      int mp[]=new int[k];
+    //   int mp[]=new int[k];
+    Map<Integer,Integer> hm=new HashMap<>();
       for(int num:arr){
         int rem=(num%k+k)%k;
-        mp[rem]++;
+        hm.put(rem,hm.getOrDefault(rem,0)+1);
       }
-      if(mp[0] % 2 !=0){
+      if(hm.getOrDefault(0,0) % 2 !=0){
         return false;
       }
       for(int i=1;i<=k/2;i++){
         int rh=k-i;
-        if(mp[rh] != mp[i]){
+        if(!hm.getOrDefault(rh,0).equals(hm.getOrDefault(i,0))){
             return false;
         }
       }
