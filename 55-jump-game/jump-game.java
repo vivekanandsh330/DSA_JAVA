64d3644d700073c4c6da5ceaee05d74fc5a4de74
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-10-04 21:52:07 +0530

    Time: 333 ms (8.46%) | Memory: 45 MB (99.01%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,23 @@
+class Solution {
+    int dp[]; 
+    int solve(int nums[], int n, int idx) {
+        if (idx == n - 1) {
+            return 1;  
+        }
+        if (dp[idx] != -1) {
+            return dp[idx];  
+        }
+        for (int i = 1; i <= nums[idx] && idx + i < n; i++) { 
+            if (solve(nums, n, idx + i) == 1) {
+                return dp[idx] = 1;  
+            }
+        }
+        return dp[idx] = 0;  
+    }
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        dp = new int[10001];  
+        Arrays.fill(dp, -1); 
+        return solve(nums, n, 0) == 1;  
+    }
+}
