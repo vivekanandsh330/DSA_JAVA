99c2fdf9c14b25c8727ee20525def33814c55251
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-10-05 23:30:22 +0530

    Time: 5 ms (96.14%) | Memory: 42.5 MB (80.53%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -3,21 +3,25 @@ class Solution {
         int n=s1.length();
         int m=s2.length();
         int mp1[]=new int[26];
-       
+        int mp2[]=new int[26];
+        if(m<n){
+            return false;
+        }
         for(int i=0;i<n;i++){
             mp1[s1.charAt(i)-'a']++;
+            mp2[s2.charAt(i)-'a']++;
         }
-        for(int j=0;j<=m-n;j++){
-             int mp2[]=new int[26];
-        for(int i=0;i<n;i++){
-             mp2[s2.charAt(i+j)-'a']++;
+        if(isMatch(mp1,mp2) == true){
+            return true;
         }
+        for(int j=1;j<=m-n;j++){
+            mp2[s2.charAt(j-1)-'a']--;
+            mp2[s2.charAt(j+n-1)-'a']++;
         if(isMatch(mp1,mp2) == true){
             return true;
         }
         }
         return false;
-
     }
     boolean isMatch(int mp[],int mp2[]){
         for(int i=0;i<26;i++){
