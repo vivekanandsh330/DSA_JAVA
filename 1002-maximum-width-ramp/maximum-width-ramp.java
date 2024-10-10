78ba395620d9ca4e75a53d0d504db67708d12a40
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-10-11 00:40:07 +0530

    Time: 3 ms (97.76%) | Memory: 55 MB (29.60%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int n=nums.length;
+        int arr[]=new int[nums.length];
+        arr[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            arr[i]=Math.max(arr[i+1],nums[i]);
+        }
+        int max=0;
+        int i=0;
+        int j=0;;
+        while(j<nums.length){
+           if(i<j && nums[i]>arr[j]){
+            i++;
+           }
+           max=Math.max(max,j-i);
+           j++;
+        }
+        return max;
+    }
+}
