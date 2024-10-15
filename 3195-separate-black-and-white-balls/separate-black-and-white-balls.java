69b5f92631bd8428dd57c0f7b2d61ae941f2c820
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-10-15 14:01:39 +0530

    Time: 7 ms (100.00%) | Memory: 45.5 MB (41.94%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -3,10 +3,10 @@ class Solution {
         long no1 = 0;
         long swap=0;
         for (int i = 0; i < s.length(); i++) {
-          if(s.charAt(i) == '0'){
-            swap+=no1;
-          }else{
+          if(s.charAt(i) == '1'){
             no1++;
+          }else{
+            swap+=no1;
           }
         }
         return swap;
