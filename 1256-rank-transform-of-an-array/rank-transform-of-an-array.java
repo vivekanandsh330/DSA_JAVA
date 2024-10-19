a69dfee67ccfb33e41f0f6726239feb7ec30826a
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-10-19 12:27:48 +0530

    Time: 40 ms (38.35%) | Memory: 63.6 MB (11.55%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int n= arr.length;
-        List<Integer> idx=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            idx.add(i);
+       int n=arr.length;
+       List<Integer> idx=new ArrayList<>();
+       for(int i=0;i<n;i++){
+        idx.add(i);
+       }
+       Collections.sort(idx,(a,b)-> arr[a]-arr[b]);
+       int ans[]=new int[n];
+       int rank=0;
+       int prev=Integer.MIN_VALUE;
+       for(int i:idx){
+        if(arr[i]>prev){
+            prev=arr[i];
+            rank++;
         }
-        Collections.sort(idx,(a,b)-> arr[a]-arr[b]);
-        int rank=0;
-        int prev=Integer.MIN_VALUE;
-        int ans[]=new int[n];
-        for(int i: idx){
-            if(prev != arr[i] ){
-                prev=arr[i];
-                rank++;
-            }
-            ans[i]=rank;
-        }
-        return ans;
+        ans[i]=rank;
+       }
+       return ans;
     }
-} 
+}
