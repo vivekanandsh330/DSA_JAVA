ffaf06212dc5e8f76c187faec8fbfc4b30d39791
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-11-18 23:03:50 +0530

    Time: 3 ms (14.82%) | Memory: 42.4 MB (24.21%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -2,7 +2,6 @@ class Solution {
     public int[] decrypt(int[] code, int k) {
         int n=code.length;
         int ans[]=new int[n];
-        
         if(k == 0){
             return new int[n];
         }
@@ -11,18 +10,17 @@ class Solution {
             for(int j=1;j<=Math.abs(k);j++){
                 if(k>0){
                     currsum+=code[(i+j)%n];
-                    
+                    ans[i]=currsum;
                 }
             }
-            ans[i]=currsum;
         }
         if(k<0){
             for(int i=0;i<n;i++){
                 int currsum=0;
                 for(int j=1;j<=Math.abs(k);j++){
                     currsum+=code[(i-j+n)%n];
+                    ans[i]=currsum;
                 }
-                ans[i]=currsum;
             }
         }
         return ans;
