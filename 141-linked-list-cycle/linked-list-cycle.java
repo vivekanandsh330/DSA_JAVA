f5ca4a292c9203745df51dc2f62980f5cb257936
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-11-29 14:47:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (30.98%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,19 +11,15 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null){
-            return false;
-        }
         ListNode s=head;
         ListNode f=head;
         while(f != null && f.next != null){
             s=s.next;
             f=f.next.next;
-            if(f==s){
-                return true;
+            if( f == s){
+            return true;
             }
         }
         return false;
-        
     }
 }
