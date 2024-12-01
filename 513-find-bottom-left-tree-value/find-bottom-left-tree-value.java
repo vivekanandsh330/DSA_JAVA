7f10ae88be3fb10be50559d5811b8cd55c4156c5
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-12-01 21:43:16 +0530

    Time: 2 ms (68.37%) | Memory: 44.4 MB (62.04%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -1,34 +1,33 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-        int maxdepth=-1;
-        int leftmostval=0;
-        void dfs(TreeNode root,int currdepth){
-        if(root == null){
-            return;
-        }
-        if(currdepth > maxdepth){
-            maxdepth=currdepth;
-            leftmostval=root.val;
-        }
-        dfs(root.left,currdepth+1);
-        dfs(root.right,currdepth+1);
-    }
     public int findBottomLeftValue(TreeNode root) {
-        dfs(root,0);
-        return leftmostval;
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        int ans = 0;
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+            ans = curr.val;
+            if (curr.right != null) {
+                q.add(curr.right);
+            }
+            if (curr.left != null) {
+                q.add(curr.left);
+            }
+        }
+        return ans;
     }
 }
