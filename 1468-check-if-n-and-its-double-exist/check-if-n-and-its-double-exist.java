8d7944ea939c61d77d58ac3f48d8e4e3b3f7f4eb
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-12-01 22:25:33 +0530

    Time: 3 ms (33.07%) | Memory: 42.6 MB (70.17%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,11 +1,28 @@
 class Solution {
+    int bs(int[] arr, int target) {
+        int l = 0;
+        int r = arr.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
     public boolean checkIfExist(int[] arr) {
-        HashSet<Integer> hm=new HashSet<>();
-        for(int i=0;i<arr.length;i++){
-            if(hm.contains(2*arr[i]) || ((arr[i] % 2 == 0) && hm.contains(arr[i]/2))){
+        int check = 0;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            check = bs(arr, 2 * arr[i]);
+            if (check != -1 && check != i) {
                 return true;
             }
-            hm.add(arr[i]);
         }
         return false;
     }
