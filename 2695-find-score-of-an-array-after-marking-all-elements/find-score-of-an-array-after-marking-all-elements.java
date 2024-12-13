6786c08dfd7dcbdfad2f4246d9ff878173e532a9
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-12-13 14:58:04 +0530

    Time: 181 ms (43.14%) | Memory: 57.5 MB (72.68%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -2,29 +2,26 @@ class Solution {
     public long findScore(int[] nums) {
         int n=nums.length;
         long fs=0;
-        boolean vis[]=new boolean[n];
-        List<int []> pair=new ArrayList<>();
-        
-        for(int i=0;i<n;i++){
-            pair.add(new int []{nums[i],i});
-        }
-        pair.sort((a,b)-> a[0] != b[0] ? a[0]-b[0]:a[1]-b[1]);
-      //  pair.sort((a,b)-> a[0] != b[0] ? Integer.compare(a[0],b[0]) : Integer.compare(a[1],a[1]));
-        for(int p[]:pair){
-            int val=p[0];
-            int idx=p[1];
-            if(!vis[idx]){
-                fs += val;
-                vis[idx]=true;
-                if(idx-1>=0) {
-                    vis[idx-1]=true;
-                }
-                if(idx+1<n){
-                 vis[idx+1]=true;
-                }
-                    
+        PriorityQueue<int []> pq=new PriorityQueue<>((a,b)->{
+            if(a[0] != b[0]){
+                return Integer.compare(a[0],b[0]);
             }
-
+            return Integer.compare(a[1],b[1]);
+        });
+        for(int i=0;i<n;i++){
+            pq.add(new int[]{nums[i],i});
+        }
+        boolean vis[]=new boolean[n];
+        while(!pq.isEmpty()){
+            int []pair=pq.poll();
+            int val=pair[0];
+            int idx=pair[1];
+            if(!vis[idx]){
+                fs+=val;
+                vis[idx]=true;
+                if(idx-1>=0) vis[idx-1]=true;
+                if(idx+1<n) vis[idx+1]=true;
+            }
         }
         return fs;
     }
