47c9441862b04002e41bf523a2e05277e183cfc5
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2024-12-28 01:48:32 +0530

    Time: 4 ms (89.46%) | Memory: 54 MB (12.70%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.java
@@ -1,16 +1,17 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
-        int ans = Integer.MIN_VALUE;
-        int m = values[0];
-
-        for (int i = 1; i < values.length; i++) {
-            if (values[i] - i + m > ans)
-                ans = values[i] - i + m;
-
-            if (values[i] + i > m)
-                m = values[i] + i;
+        int n=values.length;
+        int vec[]=new int[n];
+        vec[0]=values[0]+0;
+        for(int i=1;i<n;i++){
+            vec[i]=Math.max(values[i]+i,vec[i-1]);
         }
-
+        int ans=0;
+        for(int j=1;j<n;j++){
+            int x=values[j]-j;
+            int y=vec[j-1];
+            ans =Math.max(ans,x+y);
+        }
         return ans;
     }
 }
