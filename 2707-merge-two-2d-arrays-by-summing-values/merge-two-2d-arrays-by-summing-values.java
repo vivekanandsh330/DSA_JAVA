98f103dd969873300bd863625f937476c635d248
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2025-03-02 20:19:51 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (84.13%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -2,22 +2,34 @@ class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
         int n=nums1.length;
         int m=nums2.length;
-        HashMap<Integer,Integer> hm=new HashMap<>();
-
-        for(int []num:nums1){
-            hm.put(num[0],num[1]);
-        }
-        for(int []num:nums2){
-            hm.put(num[0],hm.getOrDefault(num[0],0)+num[1]);
-        }
-        int ans[][]=new int[hm.size()][2];
+        int i=0;
         int j=0;
-        for(int i:hm.keySet()){
-            ans[j][0]=i;
-            ans[j][1]=hm.get(i);
+        List<int []> ans=new ArrayList<>();
+        while(i<n && j <m){
+            if(nums1[i][0]<nums2[j][0]){
+                ans.add(nums1[i]);
+                i++;
+            }
+            else if(nums2[j][0]<nums1[i][0]){
+                ans.add(nums2[j]);
+                j++;
+            }
+            else{
+                ans.add(new int[]{nums1[i][0],nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            }
+        }
+
+        while(i<n){
+            ans.add(nums1[i]);
+            i++;
+        }
+        while(j<m){
+            ans.add(nums2[j]);
             j++;
         }
-        Arrays.sort(ans,(a,b)->a[0]-b[0]);
-        return ans;
+        return ans.toArray(new int[0][]);
+
     }
 }
