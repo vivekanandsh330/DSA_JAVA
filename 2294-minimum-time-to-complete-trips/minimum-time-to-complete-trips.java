03fad9a16c9217f9fbb724dfbe86670a2d30283f
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2025-04-01 01:44:02 +0530

    Time: 50 ms (59.13%) | Memory: 57.6 MB (90.87%) - LeetSync

---
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -0,0 +1,24 @@
+class Solution {
+    boolean isPossible(int[] time, int totalTrips,long giventime ){
+        long actualtime=0;
+        for(int i: time){
+            actualtime += giventime/i;
+        }
+        return actualtime>=totalTrips;
+    }
+    public long minimumTime(int[] time, int totalTrips) {
+        int n=time.length;
+        long l=1;
+        long r=(long)(Arrays.stream(time).min().getAsInt())*totalTrips;
+        while(l<r){
+            long mid=l+(r-l)/2;
+            if(isPossible(time,totalTrips,mid)){
+                r=mid;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return r;
+    }
+}
