d9615ef29a9ef97a191c56f43b3609c1f52969da
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2025-04-06 11:15:13 +0530

    Time: 101 ms (15.29%) | Memory: 57.6 MB (17.89%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,39 +1,32 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
-        int sum=0;
-        int n=nums.length;
-        for(int num: nums){
-            sum+=num;
+    int n;
+    Integer dp[][];
+    boolean solve(int idx,int target,int [] nums){
+        if(idx == n){
+            return false;
         }
-        if(sum%2 !=0){
-            return false; 
+        if(target == 0){
+            return true;
         }
-        return subsetSum(nums,sum/2,n);
+        if(dp[idx][target] != null){
+            return dp[idx][target] == 1;
+        }
+        boolean take=false;
+        if(nums[idx]<=target){
+        take=solve(idx+1,target-nums[idx],nums);
+        }
+        boolean nottake=solve(idx+1,target,nums);
+        dp[idx][target]= (take || nottake) ? 1:0;
+        return take || nottake;
     }
-    boolean subsetSum(int []nums,int target,int n){
-      Boolean dp[][]=new Boolean[n+1][target+1];
-      dp[0][0]=true;
-      for(int i=0;i<n+1;i++){
-        for(int j=0;j<target+1;j++){
-            if(i == 0){
-                dp[i][j]= false;
-            }
-            if(j == 0){
-            dp[i][j]= true;
-            }
+    public boolean canPartition(int[] nums) {
+        n=nums.length;
+        int totalval=Arrays.stream(nums).sum();
+        if(totalval % 2 != 0){
+            return false;
         }
-      }
-      for(int i=1;i<n+1;i++){
-        for(int j=1;j<target+1;j++){
-            if(nums[i-1]<=j){
-                dp[i][j]= ((dp[i-1][j-nums[i-1]]) || (dp[i-1][j]));
-            }
-            else{
-                dp[i][j]= dp[i-1][j];
-            }
-      }
-      }
-      return dp[n][target];
-        
+        dp=new Integer[n+1][(totalval/2)+1];
+        return solve(0,totalval/2,nums);
+
     }
 }
