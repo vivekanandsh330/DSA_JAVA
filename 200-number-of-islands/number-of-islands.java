d4c6b9cae2f380d36bdf222a1a083ecbb0d9364d
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2025-04-09 11:22:16 +0530

    Time: 3 ms (85.75%) | Memory: 49.6 MB (52.64%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,36 +1,30 @@
 class Solution {
-   
-    public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        if (grid == null || grid.length == 0) {
-            return 0;
+    int n;
+    int m;
+    void dfs(char [][] grid,int i,int j){
+        if(i<0 || i>=n|| j<0 || j>=m || grid[i][j] != '1'){
+            return;
         }
-        
+        grid[i][j] ='$';
+
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+
+    }
+    public int numIslands(char[][] grid) {
+        n=grid.length;
+        m=grid[0].length;
         int island=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == '1'){
+                if(grid[i][j] =='1'){
                     dfs(grid,i,j);
-                    island+=1;
+                    island++;
                 }
             }
         }
         return island;
-    }
-     void dfs(char ch[][],int i,int j){
-        int n=ch.length;
-        int m=ch[0].length;
-        int dir[][]={{1,0},{-1,0},{0,1},{0,-1}};
-        if(i<0 || i>=n || j<0 || j>=m || ch[i][j] != '1'){
-            return;
-        }
-        ch[i][j] ='0';
-        for(int d[]:dir){
-            int nr=i+d[0];
-            int nc=j+d[1];
-            dfs(ch,nr,nc);
-        }
-
     }
 }
