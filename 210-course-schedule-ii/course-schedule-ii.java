6255bfcf334f7a48d3c6cb67923c4871412b89a2
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2025-04-16 12:47:08 +0530

    Time: 8 ms (22.25%) | Memory: 46.6 MB (7.55%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,36 +1,32 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int n=prerequisites.length;
-        Map<Integer,List<Integer>> adj=new HashMap<>();
         int indgree[]=new int[numCourses];
+        HashMap<Integer,List<Integer>> adj=new HashMap<>();
         for(int i[]:prerequisites){
-            adj.computeIfAbsent(i[1],k-> new ArrayList<>()).add(i[0]);
+            adj.computeIfAbsent(i[1],k->new ArrayList<>()).add(i[0]);
             indgree[i[0]]++;
         }
-
+        List<Integer> ans=new ArrayList<>();
         Queue<Integer> q=new ArrayDeque<>();
         for(int i=0;i<numCourses;i++){
             if(indgree[i] == 0){
                 q.add(i);
             }
         }
-        int count=0;
-        int ans[]=new int[numCourses];
-        int k=0;
+
         while(!q.isEmpty()){
             int poll=q.poll();
-            count++;
-            ans[k]=poll;
-            k++;
-            for(int v:adj.getOrDefault(poll,new ArrayList<>())){
+            ans.add(poll);
+            for(int v: adj.getOrDefault(poll,new ArrayList<>())){
                 indgree[v]--;
                 if(indgree[v] == 0){
                     q.add(v);
                 }
             }
         }
-        if(count == numCourses){
-            return ans;
+        if(ans.size() == numCourses){
+            return ans.stream().mapToInt(Integer::intValue).toArray();
         }
         return new int[]{};
         
