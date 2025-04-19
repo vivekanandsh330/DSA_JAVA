e77a00cb357a11c90a677c73523e18a92d55b499
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2025-04-20 00:39:20 +0530

    Time: 64 ms (45.27%) | Memory: 57.3 MB (56.80%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -5,15 +5,15 @@ class Solution {
         long ans=0;
         for(int i=0;i<n;i++){
             int lidx=lowerBound(nums,i+1,n,lower-nums[i]);
-            int x=lidx-1-i;
+            int x=lidx-1-i;// all pair sum less than lower
             int ridx=upperBound(nums,i+1,n,upper-nums[i]);
-            int y=ridx-1-i;
-            ans +=(y-x);
+            int y=ridx-1-i;// all pair sum less than upper
+            ans +=(y-x);//all pair sum b/w lower and upper 
         }
         return ans;
     }
 
-    int lowerBound(int[] nums,int s,int e,int t){
+    int lowerBound(int[] nums,int s,int e,int t){// first element not less than x
         while(s<e){
         int mid=s+(e-s)/2;
         if(nums[mid]<t){
@@ -27,7 +27,7 @@ class Solution {
         return s;
     }
 
-    int upperBound(int[] nums,int s,int e,int t){
+    int upperBound(int[] nums,int s,int e,int t){ //first element greater than x 
         while(s<e){
             int mid=s+(e-s)/2;
             if(nums[mid]<=t){
