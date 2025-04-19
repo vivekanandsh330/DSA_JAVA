99bb25a677ec309424b642dc04c375fa98139d02
Vivekanand Singh <94869672+vivekanandsh330@users.noreply.github.com>
2025-04-20 00:36:36 +0530

    Time: 64 ms (45.27%) | Memory: 57.4 MB (56.80%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.java
@@ -1,35 +1,40 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
         Arrays.sort(nums);
-        int n = nums.length;
-        long ans = 0;
-        for (int i = 0; i < n - 1; i++) {
-            int low = lowerBound(nums, i + 1, nums.length, lower - nums[i]);
-            int up = upperBound(nums, i + 1, nums.length, upper - nums[i]);
-            ans += up - low;
+        int n=nums.length;
+        long ans=0;
+        for(int i=0;i<n;i++){
+            int lidx=lowerBound(nums,i+1,n,lower-nums[i]);
+            int x=lidx-1-i;
+            int ridx=upperBound(nums,i+1,n,upper-nums[i]);
+            int y=ridx-1-i;
+            ans +=(y-x);
         }
         return ans;
     }
 
-    int lowerBound(int[] nums, int s, int e, int t) {
-        while (s < e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] >= t) {
-                e = mid;
-            } else {
-                s = mid + 1;
-            }
+    int lowerBound(int[] nums,int s,int e,int t){
+        while(s<e){
+        int mid=s+(e-s)/2;
+        if(nums[mid]<t){
+            s=mid+1;
+        }
+        else{
+            e=mid;
+        }
+
         }
         return s;
     }
 
-    int upperBound(int[] nums, int s, int e, int t) {
-        while (s < e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] > t) {
-                e = mid;
-            } else {
-                s = mid + 1;
+    int upperBound(int[] nums,int s,int e,int t){
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]<=t){
+                s=mid+1;
+            }
+            else{
+                e=mid;
             }
         }
         return s;
